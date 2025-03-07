eca946f2041eca3dd568fd49847e9ee9de541a9a
Dawn Hajdu <dawn.hajdu@gmail.com>
2025-03-06 16:33:01 -1000

    Add checkUI function

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
+
 //Functions
 
 function addItem(e) {
@@ -25,6 +27,8 @@ function addItem(e) {
 
     itemList.appendChild(li);
 
+    checkUI();
+
     itemInput.value = '';
 
 }
@@ -46,7 +50,10 @@ function createIcon(classes) {
 
 function removeItem(e) {
     if (e.target.parentElement.classList.contains('remove-item')) {
-        e.target.parentElement.parentElement.remove();
+        if (confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+            checkUI();
+        }
     };
 }
 
@@ -56,8 +63,23 @@ function clearItems() {
     while (itemList.firstChild) {
         itemList.removeChild(itemList.firstChild);
     }
+
+    checkUI();
+}
+
+function checkUI() {
+    const items = itemList.querySelectorAll('li');
+    if (items.length === 0) {
+        clearBtn.style.display = 'none';
+        itemFilter.style.display = 'none';
+    }else {
+        clearBtn.style.display = 'block';
+        itemFilter.style.display = 'block';
+    }
 }
 //Event Listeners
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 clearBtn.addEventListener('click', clearItems);
+
+checkUI();
